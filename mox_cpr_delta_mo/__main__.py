c0e2c8c8bfc9bdc248baec8ea8bff07901fc6346
jgj-magenta-aps <jgj@magenta-aps.dk>
2018-10-02 16:13:14 +0200

    y2k handled by datetime, blackified, mute other loggers

--- mox_cpr_delta_mo/__main__.py
+++ mox_cpr_delta_mo/__main__.py
@@ -8,6 +8,7 @@
 
 import argparse
 import datetime
+import logging
 
 from mox_cpr_delta_mo import (
     mora_get_all_cpr_numbers,
@@ -17,49 +18,63 @@ from mox_cpr_delta_mo import (
     remove_cpr_subscription,
     get_all_subscribed_cprs,
 )
+from settings import MOX_LOG_LEVEL
+
+# set warning-level for all loggers
+[
+    logging.getLogger(i).setLevel(logging.WARNING)
+    for i in logging.root.manager.loggerDict
+]
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger("mox_cpr_delta_mo")
+logger.setLevel(logging.DEBUG)
+
 
 def update_cpr_subscriptions():
     "add or remove subscriptions according to mora data"
+    logger.debug("update_cpr_subscriptions started")
     must_subscribe = set(mora_get_all_cpr_numbers())
     are_subscribed = set(get_all_subscribed_cprs())
+    add_set = must_subscribe - are_subscribed
     remove_set = are_subscribed - must_subscribe
-    add_set = must_subscribe - are_subscribed 
     for pnr in remove_set:
         remove_cpr_subscription(pnr)
     for pnr in add_set:
         add_cpr_subscription(pnr)
-
+    logger.debug("update_cpr_subscriptions ended")
 
 
 def cpr_delta_update_mo(sincedate):
-    for date,citizens in get_cpr_delta_udtraek(sincedate).items():
-        fromdate = datetime.datetime.strptime(date, "%y%m%d" )
+    for date, citizens in get_cpr_delta_udtraek(sincedate).items():
+        # let python do the Y2K math
+        fromdate = datetime.datetime.strptime(date, "%y%m%d").strftime("%Y-%m-%d")
         for pnr, changes in citizens.items():
             mora_update_person_by_cprnumber(fromdate, pnr, changes)
 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+
     parser.add_argument(
-        "--update-cpr-subscriptions", 
+        "--update-cpr-subscriptions",
         help="find subscriptions for all cpr-numbers, update subscriptions for the ones "
-             "that are missing in subscrition, remove the ones missing in mora", 
-        action="store_true", 
-        default=False
+        "that are missing in subscrition, remove the ones missing in mora",
+        action="store_true",
+        default=False,
     )
     parser.add_argument(
-        "--cpr-delta-update-mo", 
+        "--cpr-delta-update-mo",
         help="retrieve data from cpr-kontoret and update mora",
-        action="store_true", 
-        default=True
+        action="store_true",
+        default=True,
     )
     parser.add_argument(
-        "--cpr-delta-since", 
+        "--cpr-delta-since",
         help="retrieve data from cpr-kontoret since this date "
-             "given as YYmmdd (180921)" ,
+        "given as YYmmdd (180921)",
         type=str,
-        default="180927"
+        default="180927",
     )
 
     args = parser.parse_args()
@@ -69,5 +84,3 @@ if __name__ == '__main__':
 
     if args.cpr_delta_update_mo:
         cpr_delta_update_mo(args.cpr_delta_since)
-
-    
